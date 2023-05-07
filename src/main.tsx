562998ae0236260e1345b6f2bb5b4d3b7a348ed3
admodev <admodevcodes@outlook.com>
2023-05-07 16:57:02 -0300

    feat(core) add router

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,23 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
 import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+// Pages
+import HomePage from './pages/Home';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomePage />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
---
+++ src/pages/Home.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react';
+
+const HomePage: React.FC  = () => {
+  return(
+    <>
+      <div>
+        <main>
+          <h1>Main Page.</h1>
+        </main>
+      </div>
+    </>
+  )
+}
+
+export default HomePage;
