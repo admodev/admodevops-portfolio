7ec8074b4e13869df4e4b52fe29e6574b26d9e51
admodev <admodevcodes@outlook.com>
2023-05-19 11:25:06 -0300

    (style): ui, refactor ui style

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,16 @@
 import * as React from "react";
 
+// styles
+import './styles/home.css';
+
 const HomePage: React.FC = () => {
   return (
     <>
       <div>
         <main>
-          <h1>Main Page.</h1>
+          <div className="titleContainer">
+            <h1>Who am i?</h1>
+          </div>
         </main>
       </div>
     </>
