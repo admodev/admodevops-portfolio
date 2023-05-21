d5ec17484f319721cf4d94a0b98114188b545ff8
admodev <admodevcodes@outlook.com>
2023-05-21 15:58:24 -0300

    style(components): add panels

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -8,8 +8,19 @@ const HomePage: React.FC = () => {
     <>
       <div>
         <main>
-          <div className="titleContainer">
-            <h1>Who am i?</h1>
+          <div className="panelsOuterContainer">
+            <div className="panelsInnerContainer">
+              <div className="panel">
+                <h1>Who am i?</h1>
+                <p>I am an engineer from Buenos Aires, Argentina. I like to build amazing projects, I enjoy working on large size projects as well as little ones too, i can make your dream come true if you let me do so,
+                with the help of my knowledge and experience i can make your project come to life.</p>
+              </div>
+              <div className="panel">
+                <h1>Who am i?</h1>
+                <p>I am an engineer from Buenos Aires, Argentina. I like to build amazing projects, I enjoy working on large size projects as well as little ones too, i can make your dream come true if you let me do so,
+                with the help of my knowledge and experience i can make your project come to life.</p>
+              </div>
+            </div>
           </div>
         </main>
       </div>
