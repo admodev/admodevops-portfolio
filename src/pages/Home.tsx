ef4c2a2e35d7099e0984913d61c052aa4cf301ef
admodev <admodevcodes@outlook.com>
2023-05-24 12:59:17 -0300

    style(page): home page, add two more cards

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -37,6 +37,31 @@ const HomePage: React.FC = () => {
               </div>
             </div>
             {/* Panel Two End */}
+            <div className="panelsInnerContainer">
+              {/* Panel Three Start */}
+              <div className="panel">
+                <h1>Who am i?</h1>
+                <p>I am an engineer from Buenos Aires, Argentina. I like to build amazing projects, I enjoy working on large size projects as well as little ones too, i can make your dream come true if you let me do so,
+                with the help of my knowledge and experience i can make your project come to life.</p>
+              </div>
+              <div className="panel">
+                <img src={uno} className="panelImage" />
+              </div>
+            </div>
+            {/* Panel Three End */}
+            {/* Panel Four Start */}
+            <div className="panelsInnerContainer">
+              {/* Panel One Start */}
+              <div className="panel">
+                <img src={uno} className="panelImage" />
+              </div>
+              <div className="panel">
+                <h1>Who am i?</h1>
+                <p>I am an engineer from Buenos Aires, Argentina. I like to build amazing projects, I enjoy working on large size projects as well as little ones too, i can make your dream come true if you let me do so,
+                with the help of my knowledge and experience i can make your project come to life.</p>
+              </div>
+            </div>
+            {/* Panel Four End */}
           </div>
         </main>
       </div>
